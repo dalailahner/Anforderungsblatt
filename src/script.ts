410865838ddbe0e09b12da948bd7307d147eabf0
dalailahner <59581086+dalailahner@users.noreply.github.com>
2024-07-10 14:20:38 +0200

    added Laufzeit & Budget

--- src/script.ts
+++ src/script.ts
@@ -1,10 +1,11 @@
+import type { Template } from "@pdfme/common";
 import { text, svg } from "@pdfme/schemas";
 import { generate } from "@pdfme/generator";
-import basePDF from "../public/basePDF.txt";
+import PDFtemplate from "./PDFtemplate.json";
 import { easepick } from "@easepick/core";
 import { KbdPlugin } from "@easepick/kbd-plugin";
-import { LockPlugin } from "@easepick/lock-plugin";
 import { RangePlugin } from "@easepick/range-plugin";
+import { LockPlugin } from "@easepick/lock-plugin";
 
 const Form: any = document.getElementsByTagName("FORM")[0];
 const checkmark: string =
@@ -20,7 +21,8 @@ const Targets = new Map<string, any>([
 const EasepickObj = new easepick.create({
   element: Form.laufzeit,
   css: ["https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css"],
-  zIndex: 10,
+  zIndex: 0,
+  readonly: false,
   RangePlugin: {
     locale: {
       one: "Tag",
@@ -29,9 +31,6 @@ const EasepickObj = new easepick.create({
   },
   LockPlugin: {
     minDate: Date.now(),
-    minDays: 1,
-    selectForward: true,
-    presets: false,
   },
   plugins: [RangePlugin, LockPlugin, KbdPlugin],
 });
@@ -60,318 +59,21 @@ Form.addEventListener("change", () => {
 });
 
 document.querySelector("#generatePDF")?.addEventListener("click", () => {
-  Form.reportValidity();
   if (Form.checkValidity()) {
     generatePDF();
+  } else {
+    Form.reportValidity();
   }
 });
 
-// TODO: parse the dates out to a readable format and include it in the generatePDF function
-console.log("EasepickObj:");
-console.log(EasepickObj.getStartDate());
-console.log(EasepickObj.getEndDate());
-
 async function generatePDF() {
-  const template = {
-    schemas: [
-      {
-        kampagnenname: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 35,
-          },
-          width: 180,
-          height: 8,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-        zielurl: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 48,
-          },
-          width: 180,
-          height: 8,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-        utmcampaign: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 61,
-          },
-          width: 180,
-          height: 8,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-        kanal: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 74,
-          },
-          width: 180,
-          height: 13,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-        sonstiges: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 92,
-          },
-          width: 180,
-          height: 13,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-        displaySN: {
-          type: "svg",
-          position: {
-            x: 25.5,
-            y: 110.5,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        displayS24: {
-          type: "svg",
-          position: {
-            x: 60.5,
-            y: 110.5,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        googleAds: {
-          type: "svg",
-          position: {
-            x: 95.5,
-            y: 110.5,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        meta: {
-          type: "svg",
-          position: {
-            x: 25.5,
-            y: 130.25,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        tiktok: {
-          type: "svg",
-          position: {
-            x: 60.5,
-            y: 130.25,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        linkedin: {
-          type: "svg",
-          position: {
-            x: 95.5,
-            y: 130.25,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        nlFreizeit: {
-          type: "svg",
-          position: {
-            x: 25.5,
-            y: 150,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        nlAktionen: {
-          type: "svg",
-          position: {
-            x: 60.5,
-            y: 150,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        albus: {
-          type: "svg",
-          position: {
-            x: 25.5,
-            y: 169.75,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        MR: {
-          type: "svg",
-          position: {
-            x: 15,
-            y: 201,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        BB: {
-          type: "svg",
-          position: {
-            x: 15,
-            y: 209,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        HPA: {
-          type: "svg",
-          position: {
-            x: 15,
-            y: 217,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        SL: {
-          type: "svg",
-          position: {
-            x: 15,
-            y: 225,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        gMR: {
-          type: "svg",
-          position: {
-            x: 105,
-            y: 201,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        gLB: {
-          type: "svg",
-          position: {
-            x: 105,
-            y: 209,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        gSS: {
-          type: "svg",
-          position: {
-            x: 105,
-            y: 217,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        gPM: {
-          type: "svg",
-          position: {
-            x: 105,
-            y: 225,
-          },
-          width: 5,
-          height: 5,
-          rotate: 0,
-          opacity: 1,
-        },
-        adText: {
-          type: "text",
-          position: {
-            x: 15,
-            y: 250,
-          },
-          width: 180,
-          height: 35,
-          fontSize: 10,
-          fontColor: "#f5f5f5",
-          fontName: "Roboto",
-          dynamicFontSize: {
-            min: 4,
-            max: 10,
-            fit: "vertical",
-          },
-        },
-      },
-    ],
-    basePdf: basePDF,
-  };
+  const template: Template = PDFtemplate;
   const plugins = { text, svg };
   const inputs = [
     {
       kampagnenname: Form.kampagnenname.value,
+      laufzeit: Form.laufzeit.value,
+      budget: Form.budget.value,
       zielurl: Form.zielurl.value,
       utmcampaign: Form.utmcampaign.value,
       kanal: Form.kanal.value,
